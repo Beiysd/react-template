035557ee2200e739a749c6c449a0800a909f3698
wxd <dev001@DESKTOP-9775V7G.(none)>
2020-08-10 15:13:05 +0800

    依赖及首页添加

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+/**
+ * @name Home
+ * @description 主页
+ * @author 方知有
+ * @date 2020/08/10
+ */
+import React, { Component } from "react"
+
+class Home extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <div>按钮</div>
+        <div>输入框</div>
+      </div>
+    )
+  }
+}
+
+export default Home
